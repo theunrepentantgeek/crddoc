1b6ec1eff08d1c069e063f6b6998c347fa8e3df4
Bevan Arps <bevan.arps@outlook.com>
2023-08-30 20:18:45 +1200

    Integrate commands into main.go

--- main.go
+++ main.go
@@ -1,75 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-
-	"github.com/go-logr/logr"
-	"github.com/go-logr/zerologr"
-	"github.com/rs/zerolog"
-	"github.com/theunrepentantgeek/crddoc/internal/config"
-	"github.com/theunrepentantgeek/crddoc/internal/generator"
-	"github.com/theunrepentantgeek/crddoc/internal/model"
+	"github.com/theunrepentantgeek/crddoc/cmd"
 )
 
 func main() {
-	log := CreateLogger()
-	cfg := &config.Config{
-		TypeFilters: []*config.Filter{
-			{
-				Exclude: "*ARM",
-				Because: "ARM types are an internal implementation detail for ASO",
-			},
-		},
-	}
-
-	pkg := model.NewPackage(cfg, log)
-
-	//err := pkg.LoadDirectory("C:\\GitHub\\azure-service-operator\\v2\\api\\network\\v1api20201101")
-	//err := pkg.LoadDirectory("C:\\GitHub\\azure-service-operator\\v2\\api\\compute\\v1api20220301")
-	err := pkg.LoadDirectory("C:\\GitHub\\azure-service-operator\\v2\\api\\containerservice\\v1api20210501")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	gen := generator.New(cfg, log)
-	err = gen.LoadTemplates("C:\\GitHub\\crddoc\\templates\\crd")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	// Render the template and write to output.md
-	f, err := os.Create("output.md")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	defer f.Close()
-
-	w := bufio.NewWriter(f)
-
-	err = gen.Generate(pkg, w)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	w.Flush()
-}
-
-func CreateLogger() logr.Logger {
-	output := zerolog.ConsoleWriter{
-		Out:        os.Stdout,
-		TimeFormat: "15:04:05.999",
-	}
-
-	zl := zerolog.New(output).
-		With().Timestamp().
-		Logger()
-
-	// Use standard interface for logging
-	zerologr.VerbosityFieldName = "" // Don't include verbosity in output
-	log := zerologr.New(&zl)
-
-	return log
+	cmd.Execute()
 }
