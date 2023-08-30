963575a15165c3a3e72724d0364f404f9ba8cf12
Bevan Arps <bevan.arps@outlook.com>
2023-08-30 20:30:04 +1200

    Adds command line arguments (#21)
    * Set up logging
    * Create document-package command
    * Integrate commands into main.go

---
+++ cmd/document_package.go
@@ -0,0 +1,125 @@
+package cmd
+
+import (
+	"bufio"
+	"context"
+	"os"
+
+	"github.com/pkg/errors"
+
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+	"github.com/theunrepentantgeek/crddoc/internal/config"
+	"github.com/theunrepentantgeek/crddoc/internal/generator"
+	"github.com/theunrepentantgeek/crddoc/internal/model"
+)
+
+func newDocumentPackageCommand(log logr.Logger) (*cobra.Command, error) {
+	options := &packageCommandOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "document-package",
+		Short: "Generate documentation for a package",
+		Long:  "Generate documentation for a package.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+			return documentPackage(ctx, args, options, log)
+		},
+	}
+
+	options.outputPath = cmd.Flags().StringP(
+		"output",
+		"o",
+		"",
+		"Write ARM resource CRDs to a single file")
+
+	return cmd, nil
+}
+
+type packageCommandOptions struct {
+	outputPath *string
+}
+
+func documentPackage(
+	ctx context.Context,
+	args []string,
+	options *packageCommandOptions,
+	log logr.Logger,
+) error {
+	if err := validateDocumentPackage(args, options); err != nil {
+		return err
+	}
+
+	cfg := &config.Config{
+		TypeFilters: []*config.Filter{
+			{
+				Exclude: "*ARM",
+				Because: "ARM types are an internal implementation detail for ASO",
+			},
+		},
+	}
+
+	pkg := model.NewPackage(cfg, log)
+	packageFolder := args[0]
+	err := pkg.LoadDirectory(packageFolder)
+	if err != nil {
+		return errors.Wrapf(err, "loading package from %q", packageFolder)
+	}
+
+	gen := generator.New(cfg, log)
+	templateFolder := "C:\\GitHub\\crddoc\\templates\\crd"
+	err = gen.LoadTemplates(templateFolder)
+	if err != nil {
+		return errors.Wrapf(err, "loading templates from %q", templateFolder)
+	}
+
+	// Render the template and write to output
+	f, err := os.Create(*options.outputPath)
+	if err != nil {
+		return errors.Wrapf(err, "creating output file %q", *options.outputPath)
+	}
+
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+
+	err = gen.Generate(pkg, w)
+	if err != nil {
+		return errors.Wrapf(err, "generating output to %q", *options.outputPath)
+	}
+
+	w.Flush()
+	return nil
+}
+
+func validateDocumentPackage(
+	args []string,
+	options *packageCommandOptions,
+) error {
+	// Error if package directory missing
+	if len(args) == 0 {
+		return errors.New("no package directory, expected exactly one")
+	}
+
+	// Error if too many package directories supplied
+	if len(args) > 1 {
+		return errors.New("multiple package directories, expected exactly one")
+	}
+
+	// Error if the package directory doesn't exist
+	packageFolder := args[0]
+	if _, err := os.Stat(packageFolder); err != nil {
+		if os.IsNotExist(err) {
+			return errors.Errorf("package directory %q does not exist", packageFolder)
+		}
+
+		return errors.Wrapf(err, "checking package directory %q", packageFolder)
+	}
+
+	// Error if no output path supplied
+	if options.outputPath == nil || *options.outputPath == "" {
+		return errors.New("no output path supplied")
+	}
+
+	return nil
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,23 +1,61 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
+	"github.com/go-logr/logr"
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "crddoc",
-	Short: "crddoc is a generator for CRD documentation",
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
-}
-
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	log := CreateLogger()
+	root, err := newRootCommand(log)
+	if err != nil {
+		log.Error(err, "failed to create root command")
 		os.Exit(1)
 	}
+
+	if err := root.Execute(); err != nil {
+		log.Error(err, "failed to execute command")
+		os.Exit(1)
+	}
+}
+
+var (
+	verbose bool
+)
+
+func newRootCommand(log logr.Logger) (*cobra.Command, error) {
+	rootCmd := &cobra.Command{
+		Use:              "crddoc",
+		Short:            "crddoc is a generator for CRD documentation",
+		TraverseChildren: true,
+		SilenceErrors:    true, // We show errors ourselves using our logger
+		SilenceUsage:     true, // Let users ask for usage themselves
+	}
+
+	rootCmd.PersistentFlags().BoolVar(&verbose, "verbose", false, "Enable verbose logging")
+
+	rootCmd.Flags().SortFlags = false
+
+	cmds := []func(logr.Logger) (*cobra.Command, error){
+		newDocumentPackageCommand,
+	}
+
+	for _, f := range cmds {
+		cmd, err := f(log)
+		if err != nil {
+			return rootCmd, err
+		}
+
+		rootCmd.AddCommand(cmd)
+	}
+
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		if verbose {
+			verboseLogging()
+		}
+	}
+
+	return rootCmd, nil
 }
