1c89d96e4ff69e07f6e42b0f8a89da00a7201300
Bevan Arps <bevan.arps@outlook.com>
2023-10-07 21:00:43 +1300

    Extract editor functionality (#40)

--- internal/functions/apply_editors.go
+++ internal/functions/apply_editors.go
@@ -1,9 +1,5 @@
 package functions
 
 func (f *Functions) applyEdits(content string) string {
-	for _, editor := range f.cfg.Editors {
-		content = editor.Replace(content)
-	}
-
-	return content
+	return f.editors.Replace(content)
 }
---
+++ internal/texteditor/text_editor_list.go
@@ -0,0 +1,32 @@
+package texteditor
+
+import "github.com/theunrepentantgeek/crddoc/internal/config"
+
+type TextEditorList struct {
+	editors []*Editor
+}
+
+func New(cfg *config.Config) (*TextEditorList, error) {
+	editors := make([]*Editor, 0, len(cfg.Editors))
+	for _, f := range cfg.Editors {
+		editor, err := NewEditor(f)
+		if err != nil {
+			return nil, err
+		}
+
+		editors = append(editors, editor)
+	}
+
+	return &TextEditorList{
+		editors: editors,
+	}, nil
+}
+
+func (list *TextEditorList) Replace(input string) string {
+	result := input
+	for _, editor := range list.editors {
+		result = editor.Replace(result)
+	}
+
+	return result
+}
