3b3dbc9876ee8e282e33be8e338e8db6d2166a89
Bevan Arps <bevan.arps@outlook.com>
2025-02-22 08:31:00 +0000

    Update test file

--- internal/packageloader/testdata/party_types.go
+++ internal/packageloader/testdata/party_types.go
@@ -2,10 +2,24 @@
 // +versionName=v1beta3
 package v1
 
+import (
+	"errors"
+	"fmt"
+
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
+
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/conditions"
+)
+
 // PartyResource represents a person, company or organization
 type PartyResource struct {
-	Spec   PartyResourceSpec   `json:"spec"`
-	Status PartyResourceStatus `json:"status"`
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              PartyResourceSpec   `json:"spec"`
+	Status            PartyResourceStatus `json:"status"`
 }
 
 type PartyResourceSpec struct {
@@ -14,7 +28,8 @@ type PartyResourceSpec struct {
 }
 
 type PartyResourceStatus struct {
-	Name string `json:"name"`
+	Name       string                 `json:"name"`
+	Conditions []conditions.Condition `json:"conditions,omitempty"`
 }
 
 type PartyReference struct {
@@ -28,3 +43,13 @@ const (
 	PartyKindCompany      = PartyKind("Company")
 	PartyKindOrganization = PartyKind("Organization")
 )
+
+func (ref *PartyReference) Validate() error {
+	if ref.Name == "" {
+		return kerrors.NewInvalid("name", ref.Name)
+	}
+
+	msg := fmt.Sprintf("PartyReference %s is not a valid kind", ref.Name)
+
+	return errors.New(msg)
+}
