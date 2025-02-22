e67d5222aa19a7c615beb49c81f9a74ef98af5b3
Bevan Arps <bevan.arps@outlook.com>
2025-02-22 15:06:32 +1300

    Update createLink function

--- internal/functions/create_link.go
+++ internal/functions/create_link.go
@@ -1,6 +1,12 @@
 package functions
 
-import "github.com/theunrepentantgeek/crddoc/internal/model"
+import (
+	"bytes"
+
+	"github.com/theunrepentantgeek/crddoc/internal/model"
+)
+
+var createLinkLoggedTypes = map[model.TypeReference]bool{}
 
 // createLink creates a link to the passed TypeReference, returning an empty string
 // if no link is required.
@@ -10,7 +16,41 @@ func (f *Functions) createLink(ref *model.TypeReference) string {
 		return "#" + ref.ID()
 	}
 
-	// TODO: External links
+	if ref.ImportPath() != "" {
+		return f.createExternalLink(ref)
+	}
+
+	return ""
+}
+
+func (f *Functions) createExternalLink(ref *model.TypeReference) string {
+	// Check if we have a template for this import path
+	if tmpl, ok := f.externalLinks[ref.ImportPath()]; ok {
+		var buffer bytes.Buffer
+
+		err := tmpl.Execute(&buffer, ref)
+		if err != nil {
+			f.log.Error(
+				err,
+				"Failed to generate external link",
+				"importPath", ref.ImportPath())
+
+			return ""
+		}
+
+		return buffer.String()
+	}
+
+	// Log a warning that we don't have configuration to generate an external link, but only once
+	// per type. This gives users enough information to configure links if they choose.
+	if _, ok := createLinkLoggedTypes[*ref]; !ok {
+		createLinkLoggedTypes[*ref] = true
+
+		f.log.V(0).Info(
+			"Missing external link configuration",
+			"package", ref.Package(),
+			"importPath", ref.ImportPath())
+	}
 
 	return ""
 }
