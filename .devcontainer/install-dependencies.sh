bda0176bc9dca7b1bca4b6a7c6f800b3484be84c
Bevan Arps <bevan.arps@outlook.com>
2025-04-03 21:18:05 +1300

    Update version of task

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -178,7 +178,7 @@ fi
 write-verbose "Checking for $TOOL_DEST/go-task"
 if should-install "$TOOL_DEST/task"; then 
     write-info "Installing go-task"
-    curl -sL "https://github.com/go-task/task/releases/download/v3.39.2/task_${os}_${arch}.tar.gz" | tar xz -C "$TOOL_DEST" task
+    curl -sL "https://github.com/go-task/task/releases/download/v3.42.1/task_${os}_${arch}.tar.gz" | tar xz -C "$TOOL_DEST" task
 fi
 
 # Install oh-my-posh
