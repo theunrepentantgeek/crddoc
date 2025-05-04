4c8aeb3b75e4009d3b9668b8980bb8b3e6c11d65
Bevan Arps <bevan.arps@outlook.com>
2025-05-04 17:37:05 +1200

    Use helper function to index declarations

--- internal/model/package_builder.go
+++ internal/model/package_builder.go
@@ -19,35 +19,35 @@ type PackageBuilder struct {
 // Build creates a new Package from the builder.
 func (b *PackageBuilder) Build() *Package {
 	result := &Package{
-		cfg:       b.Config,
-		resources: make(map[string]*Resource, len(b.Resources)),
-		objects:   make(map[string]*Object, len(b.Objects)),
-		enums:     make(map[string]*Enum, len(b.Enums)),
-		ranks:     make(map[string]int, len(b.Resources)+len(b.Objects)+len(b.Enums)),
-		metadata:  b.Metadata,
-		log:       b.Log,
+		cfg:      b.Config,
+		ranks:    make(map[string]int, len(b.Resources)+len(b.Objects)+len(b.Enums)),
+		metadata: b.Metadata,
+		log:      b.Log,
 	}
 
-	// Add all resources
-	for _, d := range b.Resources {
-		d.SetPackage(result)
-		result.resources[d.Name()] = d
-	}
-
-	// Add all objects
-	for _, d := range b.Objects {
-		d.SetPackage(result)
-		result.objects[d.Name()] = d
-	}
-
-	// Add all enums
-	for _, d := range b.Enums {
-		d.SetPackage(result)
-		result.enums[d.Name()] = d
-	}
+	result.resources = indexByName(b.Resources, result)
+	result.objects = indexByName(b.Objects, result)
+	result.enums = indexByName(b.Enums, result)
 
 	result.catalogCrossReferences()
 	result.calculateRanks()
+
+	return result
+}
+
+type indexable interface {
+	Name() string
+	SetPackage(pkg *Package)
+}
+
+// indexByName creates a map of items indexed by their name.
+func indexByName[N indexable](items []N, pkg *Package) map[string]N {
+	result := make(map[string]N, len(items))
+
+	for _, item := range items {
+		item.SetPackage(pkg)
+		result[item.Name()] = item
+	}
 
 	return result
 }
