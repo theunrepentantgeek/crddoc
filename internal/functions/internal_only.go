007d4da55beed305e1dc9cb59529ad662f4a5d97
Bevan Arps <bevan.arps@outlook.com>
2025-05-07 08:01:56 +0000

    Address linters

--- internal/functions/internal_only.go
+++ internal/functions/internal_only.go
@@ -2,13 +2,15 @@ package functions
 
 import "github.com/theunrepentantgeek/crddoc/internal/model"
 
-// internalOnly selects only properties with types decared in the same package
+// internalOnly selects only properties with types decared in the same package.
 func (f *Functions) internalOnly(properties []*model.Property) []*model.Property {
 	result := make([]*model.Property, 0, len(properties))
+
 	for _, prop := range properties {
 		if _, found := f.pkg.Declaration(prop.Type.ID()); !found {
 			result = append(result, prop)
 		}
 	}
+
 	return result
 }
